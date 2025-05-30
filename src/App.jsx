0f13be4560f4338cc9c4419551c2c2c77643e453
Matheus dos Santos Soares <maverick17xd@gmail.com>
2025-05-30 15:10:44 -0300

    :sparkles: feat: add iphone model, lights, loader and modelview components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
-const  App = () => {
+import Model from "./components/Model";
+const App = () => {
   return (
     <main className="bg-black">
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
-  )
-}
+  );
+};
 
 export default App;
